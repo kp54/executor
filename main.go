852a2a20de2966a9b85bf49a2c249f03abef9103
kp54 <kangpang65@gmail.com>
2018-09-23 12:24:08 +0900

    最初から完成

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"io"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		os.Stdout.WriteString("usage: executor TARGET EXIT [ARGS...]")
+		return
+	}
+
+	cmd := exec.Command(os.Args[1], os.Args[3:]...)
+
+	cstdin, err := cmd.StdinPipe()
+	if err != nil {
+		panic(err)
+	}
+	defer cstdin.Close()
+	cstdout, err := cmd.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+	defer cstdout.Close()
+	cstderr, err := cmd.StderrPipe()
+	if err != nil {
+		panic(err)
+	}
+	defer cstderr.Close()
+
+	// stdin, stdout, stderr をつなぐ (こんな雑でいいのか?)
+	go func() {
+		for {
+			io.Copy(cstdin, os.Stdin)
+		}
+	}()
+	go func() {
+		for {
+			io.Copy(os.Stdout, cstdout)
+		}
+	}()
+	go func() {
+		for {
+			io.Copy(os.Stderr, cstderr)
+		}
+	}()
+
+	// SIGTERM をトラップ
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGTERM)
+
+	// onSIGTERM
+	go func() {
+		for {
+			<-ch
+			// os.Stdin がバッファされてる前提 (何か入ってたら死ぬよねこれ)
+			io.WriteString(cstdin, os.Args[2]+"\n")
+		}
+	}()
+
+	cmd.Run()
+}
